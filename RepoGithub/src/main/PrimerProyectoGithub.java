1d122f90080b57150de59e528d7c3ab9c132f6c5
FRANZIE <FRANZIE@DESKTOP-6O3DPBK>
2018-02-21 01:04:47 -0300

    Mensaje Primer Proyecto

---
+++ RepoGithub/src/main/PrimerProyectoGithub.java
@@ -0,0 +1,13 @@
+package main;
+/**
+ * @author Manuel García
+ */
+public class PrimerProyectoGithub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo desde mi github");
+
+	}
+
+}
